6456d5a3ed27c0546821562669b33c11e53c2192
Arunscape <Arunscape@users.noreply.github.com>
2016-04-19 16:37:53 -0600

    added documentation

--- matrix.py
+++ matrix.py
@@ -1,13 +1,15 @@
-boolean = False
+
 from random import randint
-import terminalsize
+import terminalsize #this is the other file terminalsize.py
 string = ""
+#start an infinite loop
+boolean = False
 while boolean == False:
-  (width, height) = terminalsize.getTerminalSize()
+  (width, height) = terminalsize.getTerminalSize() #this allows you to print exactly enough ones and zeros to fill one line
   for i in range(width-1):
 
     x = randint(0,1)
     string += str(x)
-  print("\033[1;32;40m %s" %string)
-  string = ""
+  print("\033[1;32;40m %s" %string) #print in the colour green
+  string = "" #clearing the string allows a new line of ones and zeroes to be generated
 
